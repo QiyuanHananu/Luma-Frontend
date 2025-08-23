c828fdb077046be54add79a7e1cdda49b55b8562
QiyuanHan <u7975384@anu.edu.au>
2025-08-23 22:59:47 +1000

    Initial Commit

---
+++ LumaTests/LumaTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumaTests.swift
+//  LumaTests
+//
+//  Created by Han on 23/8/2025.
+//
+
+import Testing
+@testable import Luma
+
+struct LumaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
