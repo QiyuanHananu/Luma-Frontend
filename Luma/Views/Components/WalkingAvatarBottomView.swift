6461eb71d20a6607a0676a770d4cea902543ce05
u7976905 <u7976905@anu.edu.au>
2025-08-27 13:13:51 +1000

    Add walking avatar view at screen bottom

---
+++ Luma/Views/Components/WalkingAvatarBottomView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct WalkingAvatarBottomView: View {
+    @State private var positionX: CGFloat = 0
+    @State private var direction: CGFloat = 1
+
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    let avatarSize: CGFloat = 80
+
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea()
+
+            // Avatar 走动位置
+            Image(systemName: "person.fill")
+                .resizable()
+                .frame(width: avatarSize, height: avatarSize)
+                .foregroundColor(.blue)
+                .offset(x: positionX, y: screenHeight / 2 - avatarSize) // ➜ 固定在底部
+                .animation(.linear(duration: 0.02), value: positionX)
+        }
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { _ in
+                withAnimation {
+                    positionX += direction * 1.5
+
+                    // 到达屏幕边缘时反向
+                    if positionX > (screenWidth / 2 - avatarSize / 2) {
+                        direction = -1
+                    } else if positionX < -(screenWidth / 2 - avatarSize / 2) {
+                        direction = 1
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct WalkingAvatarBottomView_Previews: PreviewProvider {
+    static var previews: some View {
+        WalkingAvatarBottomView()
+    }
+}
